decf19fadb402a4e15cb36d43d5c51a882aac95b
Rob Voigt <code@ravoigt.com>
2024-05-13 02:35:29 +0200

    Remove token retrieval logic from headers.add_token_to_request_headers and make it only handle mutation of the dict.

--- src/iaptoolkit/headers.py
+++ src/iaptoolkit/headers.py
@@ -4,10 +4,8 @@ from kvcommon import logger
 
 from iaptoolkit.constants import GOOGLE_IAP_AUTH_HEADER
 from iaptoolkit.constants import GOOGLE_IAP_AUTH_HEADER_PROXY
-from iaptoolkit.tokens import get_token_for_google_service_account
 from iaptoolkit.tokens.structs import ResultAddTokenHeader
 from iaptoolkit.utils.urls import is_url_safe_for_token
-from iaptoolkit.vars import IAPTOOLKIT_USE_AUTH_HEADER
 from iaptoolkit.vars import GOOGLE_IAP_CLIENT_ID
 
 LOG = logger.get_logger("iaptk")
@@ -39,33 +37,43 @@ def sanitize_request_headers(headers: dict) -> dict:
     return log_safe_headers
 
 
-def add_token_to_request_headers(request_headers: dict, use_oauth2: bool, iap_client_id: str) -> bool:
+def add_token_to_request_headers(
+    request_headers: dict, id_token: str, use_auth_header: bool = False
+) -> dict:
     """
     Adds Bearer token to headers dict. Modifies dict in-place.
     Returns True if added token is a fresh one, or False if token is from cache
+
+    Params:
+        request_headers: Existing dict of request headers to mutate
+        use_auth_header: If True (default), use 'Authorization' header instead of 'Proxy-Authorization'
+
+    Returns:
+        True if the added token is newly-retrieved
+        False if the added token is from cache
     """
     # TODO: Make this less google-specific, or move it to a google-specific implementation
-    # TODO: oauth2
 
-    token_refresh_struct = get_token_for_google_service_account(iap_client_id=iap_client_id)
-    id_token: str = token_refresh_struct.id_token
     auth_header_str = "Bearer {}".format(id_token)
 
+    # Use 'Proxy-Authorization' header by default
     auth_header_key = GOOGLE_IAP_AUTH_HEADER_PROXY
-    # Don't override an existing authorization header if there is one
-    # Google IAP supports passing the token in 'Proxy-Authorization' header if `Authorization` is already in use
-    if IAPTOOLKIT_USE_AUTH_HEADER:
+
+    if use_auth_header:
+        # Default to use of 'Authorization' header instead of 'Proxy-Authorization'
+
+        # Don't override an existing authorization header if there is one
+        # Google IAP supports passing the token in 'Proxy-Authorization' header if
+        # `Authorization` is already in use
         if GOOGLE_IAP_AUTH_HEADER not in request_headers:
             request_headers[GOOGLE_IAP_AUTH_HEADER] = auth_header_str
             auth_header_key = GOOGLE_IAP_AUTH_HEADER
         else:
             LOG.debug(
-                "IAPTOOLKIT_USE_AUTH_HEADER is set but 'Authorization' header already exists. "
+                "'use_auth_header' is True but 'Authorization' header already exists. "
                 "Adding IAP token to Proxy-Authorization header only."
             )
 
     request_headers[auth_header_key] = auth_header_str
 
-    return token_refresh_struct.token_is_new
-
-
+    return request_headers

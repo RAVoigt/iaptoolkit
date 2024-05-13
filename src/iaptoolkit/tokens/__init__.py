ade3082dd12b03ad105efa9a5a42fdc9ae044a76
Rob Voigt <code@ravoigt.com>
2024-05-13 02:36:10 +0200

    Remove tokens.get_token_for_google_service_account().

--- src/iaptoolkit/tokens/__init__.py
+++ src/iaptoolkit/tokens/__init__.py
@@ -15,16 +15,7 @@ from .service_account import GoogleServiceAccount
 LOG = logger.get_logger("iaptk")
 
 
-def get_token_for_google_service_account(iap_client_id):
-    try:
-        return GoogleServiceAccount(iap_client_id).get_token()
-    except ServiceAccountTokenException as ex:
-        LOG.debug(ex)
-        raise
-
-
 __all__ = [
-    "get_token_for_google_service_account",
     "TokenStruct",
     "TokenRefreshStruct",
     # "TokenStructOAuth2",  # TODO: OAuth2

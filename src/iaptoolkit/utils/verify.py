a56bf46a2de38e9dfafb6c42db0dff3083e32a1c
Rob Voigt <code@ravoigt.com>
2025-03-20 00:08:36 +0100

    Lint cleanup with black.

--- src/iaptoolkit/utils/verify.py
+++ src/iaptoolkit/utils/verify.py
@@ -18,6 +18,7 @@ class GoogleIAPKeys:
     Retrieve Google's public keys for JWT verification and record the timestamp at retrieval.
     If the retrieval was >5m ago (default),  refresh the keys in case of rotation or expiry
     """
+
     _retrieved_timestamp: datetime.datetime
     _key_ttl_seconds: int = 300  # 5 mins
     _certs: dict
@@ -32,20 +33,24 @@ class GoogleIAPKeys:
         response.raise_for_status()
 
         try:
-            certs = json.loads(response.text)['keys']
+            certs = json.loads(response.text)["keys"]
         except json.JSONDecodeError as ex:
             raise GooglePublicKeyException(f"Decode error in JSON retrieved for Google Public Keys: {ex}")
         except KeyError as ex:
             raise GooglePublicKeyException(f"KeyError with JSON retrieved for Google Public Keys: {ex}")
         if not certs:
-            raise GooglePublicKeyException(f"Failed to retrieve JSON public keys from Google at: '{GOOGLE_IAP_PUBLIC_KEY_URL}'")
+            raise GooglePublicKeyException(
+                f"Failed to retrieve JSON public keys from Google at: '{GOOGLE_IAP_PUBLIC_KEY_URL}'"
+            )
 
         self._retrieved_timestamp = datetime.datetime.now(tz=datetime.UTC)
         self._certs = certs
 
     @property
     def should_refresh(self) -> bool:
-        if self._retrieved_timestamp > datetime.datetime.now() - datetime.timedelta(seconds=self._key_ttl_seconds):
+        if self._retrieved_timestamp > datetime.datetime.now() - datetime.timedelta(
+            seconds=self._key_ttl_seconds
+        ):
             return False
         return True
 

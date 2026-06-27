398d10d3f6cf1dca3a6fbfea38fad536ae8a6968
Rob Voigt <code@ravoigt.com>
2026-06-27 17:45:48 +0200

    Bugfix: Remove lingering httpx import and URL IAP utils that no longer work due to upstream changes by G.

--- src/iaptoolkit/utils/urls.py
+++ src/iaptoolkit/utils/urls.py
@@ -1,15 +1,9 @@
-from dataclasses import dataclass
-import httpx
-import requests
 import typing as t
-from urllib.parse import parse_qs
 from urllib.parse import ParseResult
-from urllib.parse import urlparse
 
 from kvcommon import logger
 from kvcommon.urls import get_netloc_without_port_from_url_parts
 
-from iaptoolkit.exceptions import IAPClientIDException
 from iaptoolkit.exceptions import InvalidDomain
 
 LOG = logger.get_logger("iaptk")
@@ -49,71 +43,3 @@ def is_url_safe_for_token(
             return True
 
     return False
-
-
-@dataclass(kw_only=True)
-class IAPURLState:
-    url: str
-    protected: bool = False
-    iap_audience: str | None = None
-
-
-def _determine_iap_state_from_response(response: requests.Response | httpx.Response, url: str):
-    # This approach may not be reliable - Undocumented?
-
-    iap_audience = None
-    requires_iap = False
-
-    if response.status_code == 302:
-        location = response.headers.get("location")
-        qs = str(urlparse(location).query)
-        query = parse_qs(qs) or {}
-        if "client_id" in query:
-            iap_audience = str(query["client_id"][0])
-            requires_iap = True
-
-    return IAPURLState(url=url, protected=requires_iap, iap_audience=iap_audience)
-
-
-def get_url_iap_state(url: str) -> IAPURLState:
-    response = requests.get(url, allow_redirects=False)
-    return _determine_iap_state_from_response(response, url)
-
-
-async def get_url_iap_state_async(url: str) -> IAPURLState:
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, follow_redirects=False)
-        return _determine_iap_state_from_response(response, url)
-
-
-def is_url_iap_protected(url: str) -> bool:
-    url_state: IAPURLState = get_url_iap_state(url)
-    return url_state.protected
-
-async def is_url_iap_protected_async(url: str) -> bool:
-    url_state: IAPURLState = await get_url_iap_state_async(url)
-    return url_state.protected
-
-
-def _handle_url_state_for_audience(url_state: IAPURLState) -> str | None:
-    if not url_state.protected:
-        raise IAPClientIDException(f"URL does not appear to be IAP-protected: '{url_state.url}'")
-
-    iap_audience = url_state.iap_audience
-    if not iap_audience:
-        raise IAPClientIDException(
-            f"No client_id returned in redirect for query when trying to retrieve IAP Client ID for url: '{url_state.url}'"
-        )
-    return iap_audience
-
-
-def get_iap_audience_for_url(url: str) -> str | None:
-    url_state: IAPURLState = get_url_iap_state(url)
-
-# TODO: Legacy terminology - Deprecate
-get_iap_client_id_for_url = get_iap_audience_for_url
-
-
-async def get_iap_audience_for_url_async(url: str) -> str | None:
-    url_state: IAPURLState = await get_url_iap_state_async(url)
-    return _handle_url_state_for_audience(url_state)
